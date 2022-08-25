6addcb3bfb08eb8af5e3d5a4bb04e6b92a89f1d1
unknown <ebranafas@gmail.com>
2022-08-25 18:23:13 +0430

    add hover effect to nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,48 +11,48 @@ function Navbar() {
     <>
       <div className="bg-white">
         <SectionContainer>
-          <nav className="flex flex-row items-center justify-between py-5">
+          <nav className="flex flex-row items-center justify-between">
             <a href="/" className="w-[120px] sm:w-[150px]">
               <img src={logo} className="w-full" alt="" />
             </a>
             <div className="flex flex-row items-center">
               <ul className="hidden md:flex flex-row items-center">
-                <li className="mr-5 opacity-80 hover:opacity-100 hover:underline hover:font-semibold transition-none hover:underline-offset-[10px]">
+                <li className="mr-7 relative opacity-80 hover:opacity-100">
                   <a
                     href="https://go.com"
-                    className="capitalize tracking-wider text-sm"
+                    className="capitalize text-sm opacity-80 block py-8 after:contents-['*'] after:absolute after:bottom-0 after:left-0 after:right-0 after:w-full after:h-1 after:bg-gradient-to-r after:from-green after:to-blue after:opacity-0 hover:opacity-100 hover:after:opacity-100"
                   >
                     Home
                   </a>
                 </li>
-                <li className="mr-5 opacity-80 hover:opacity-100 hover:underline hover:font-semibold transition-none hover:underline-offset-[10px]">
+                <li className="mr-7 relative opacity-80 hover:opacity-100">
                   <a
                     href="https://go.com"
-                    className="capitalize tracking-wider text-sm"
+                    className="capitalize text-sm opacity-80 block py-8 after:contents-['*'] after:absolute after:bottom-0 after:left-0 after:right-0 after:w-full after:h-1 after:bg-gradient-to-r after:from-green after:to-blue after:opacity-0 hover:opacity-100 hover:after:opacity-100"
                   >
                     About
                   </a>
                 </li>
-                <li className="mr-5 opacity-80 hover:opacity-100 hover:underline hover:font-semibold transition-none hover:underline-offset-[10px]">
+                <li className="mr-7 relative opacity-80 hover:opacity-100">
                   <a
                     href="https://go.com"
-                    className="capitalize tracking-wider text-sm"
+                    className="capitalize text-sm opacity-80 block py-8 after:contents-['*'] after:absolute after:bottom-0 after:left-0 after:right-0 after:w-full after:h-1 after:bg-gradient-to-r after:from-green after:to-blue after:opacity-0 hover:opacity-100 hover:after:opacity-100"
                   >
                     contact
                   </a>
                 </li>
-                <li className="mr-5 opacity-80 hover:opacity-100 hover:underline hover:font-semibold transition-none hover:underline-offset-[10px]">
+                <li className="mr-7 relative opacity-80 hover:opacity-100">
                   <a
                     href="https://go.com"
-                    className="capitalize tracking-wider text-sm"
+                    className="capitalize text-sm opacity-80 block py-8 after:contents-['*'] after:absolute after:bottom-0 after:left-0 after:right-0 after:w-full after:h-1 after:bg-gradient-to-r after:from-green after:to-blue after:opacity-0 hover:opacity-100 hover:after:opacity-100"
                   >
                     blog
                   </a>
                 </li>
-                <li className="mr-5 opacity-80 hover:opacity-100 hover:underline hover:font-semibold transition-none hover:underline-offset-[10px]">
+                <li className="mr-7 relative opacity-80 hover:opacity-100">
                   <a
                     href="https://go.com"
-                    className="capitalize tracking-wider text-sm"
+                    className="capitalize text-sm opacity-80 block py-8 after:contents-['*'] after:absolute after:bottom-0 after:left-0 after:right-0 after:w-full after:h-1 after:bg-gradient-to-r after:from-green after:to-blue after:opacity-0 hover:opacity-100 hover:after:opacity-100"
                   >
                     careers
                   </a>
